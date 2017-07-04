1bfb2411d83292bd59f39a328380a63e06594897
zpf0918 <zhanggongzifei@163.com>
2017-07-04 10:34:40 +0800

    base testing

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -21,7 +21,10 @@ class Parking < ApplicationRecord
 
   def calculate_amount
     if self.amount.blank? && self.start_at.present? && self.end_at.present?
-      self.amount = 9876
+      # self.amount = 9876
+      if duration <= 60
+        self.amount = 200
+      end
     end
   end
 
--- spec/models/parking_spec.rb
+++ spec/models/parking_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe Parking, type: :model do
        expect( parking ).to_not be_valid
      end
    end
+
+  describe ".calculate_amount" do
+
+    it "60 mins should be ¥2" do
+      t = Time.now
+      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 60.minutes )
+      parking.calculate_amount
+      expect( parking.amount ).to eq(200)
+    end
+    
+    it "30 mins should be ¥2" do
+      t = Time.now
+      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 30.minutes )
+      parking.calculate_amount
+      expect(parking.amount).to eq(200)
+    end
+  end
 end
